075ca991c6a0327c936fac131aa553739b3e575f
kushalkumar09 <kushal1748.be21@chitkara.edu.in>
2022-11-05 18:27:52 +0530

    Done

---
+++ javascript/script.js
@@ -0,0 +1,7 @@
+// =============== typing animation =============//
+var typed = new Typed(".typing",{
+  strings:["Web Designer","Web Developer","Software Developer"],
+  typeSpeed:100,
+  backSpeed:50,
+  loop:true
+})
