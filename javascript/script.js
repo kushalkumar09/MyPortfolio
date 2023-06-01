915f0c5ec1a7a0d47a4c6133f2c1ea03d237d63c
kushalkumar09 <kushal1748.be21@chitkara.edu.in>
2023-06-01 15:18:07 +0530

    typing bug fixed

--- javascript/script.js
+++ javascript/script.js
@@ -1,6 +1,6 @@
 // =============== typing animation =============//
 var typed = new Typed(".typing", {
-  strings: ["hello", "Web Developer", "Software Developer"],
+  strings: ["Web Designer", "Software Developer", "Web Developer"],
   typeSpeed: 100,
   backSpeed: 50,
   loop: true,
