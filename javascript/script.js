06754ca3ee2ef9f9f6071872ee170837c8aa6868
kushalkumar09 <101243584+kushalkumar09@users.noreply.github.com>
2023-08-11 14:43:54 +0530

    Add files via upload
    initial commit

---
+++ javascript/script.js
@@ -0,0 +1,7 @@
+// =============== typing animation =============//
+var typed = new Typed(".typing", {
+  strings: ["Web Designer", "Software Developer", "Web Developer"],
+  typeSpeed: 100,
+  backSpeed: 50,
+  loop: true,
+});
